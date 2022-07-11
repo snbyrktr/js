1bed9b4536cac2c2be7340ad2e67723c2750d20e
sena <snbyrktr@hotmail.com>
2022-07-11 19:23:05 +0300

    first commit

---
+++ sayi.js
@@ -0,0 +1,8 @@
+let a=prompt("sayı giriniz: ")
+let b=prompt("sayı giriniz: ")
+let c=prompt("sayı giriniz: ")
+
+console.log("a sayısı büyük mü? : ", (a>b) && (a>c))
+console.log("b sayısı büyük mü? : ", (a<b) && (b>c))
+console.log("c sayısı büyük mü? : ", (c>b) && (a<c))
+
